154ed425ee2a468a2a1794631da557d3e4eb2350
Md. Shahbaz Kamal Chowdhury <shahbazkamal384@gmail.com>
2025-08-20 13:37:58 +0600

    watched L 15: learned about back easing

---
+++ pages/05a_back-easing/script.js
@@ -0,0 +1,31 @@
+import gsap from "gsap";
+
+const tabs = document.querySelectorAll(".tab");
+const indicator = document.querySelector(".indicator");
+const tabRow = document.querySelector(".tab-row");
+
+const updateIndicator = (target) => {
+  const tabBounds = target.getBoundingClientRect(); //
+  const rowBounds = tabRow.getBoundingClientRect(); // where the clicked tab is inside the row
+
+  const width = tabBounds.width;
+  const offset = tabBounds.left - rowBounds.left;
+
+  gsap.to(indicator, {
+    x: offset,
+    width: width,
+    duration: 1.4,
+    ease: "back.out(1.7)",
+    duration: 0.4,
+  });
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      tabs.forEach((t) => t.classList.remove("active"));
+      tab.classList.add("active");
+      updateIndicator(tab);
+    });
+  });
+};
+
+updateIndicator(document.querySelector(".tab.active"));
